178dc1d5a7f548330d318a70ff703c08f2f2e064
wbposha <135736179+wbposha@users.noreply.github.com>
2023-08-17 01:04:26 +0400

    Update Radio.java

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -1,16 +1,25 @@
 package org.example;
 
 public class Radio {
-  private int currentVolume;
-  private int currentRadioStation;
+    private int currentVolume;
+    private int currentRadioStation;
 
-    public void increaseVolume() {
-        if (currentVolume < 100) {
-            currentVolume = currentVolume + 1;
-        }
+    private int maxRadioStation = 9;
+    private int minRadioStation = 0;
+
+    private int maxVolume = 99;
+    private int minVolume = 0;
+
+    public Radio(int maxRadioStation, int minRadioStation ) {
+        this.maxRadioStation = maxRadioStation;
+        this.minRadioStation = minRadioStation;
     }
 
-    public void setCurrentRadioStation(int newCurrentRadioStation){
+   public Radio(int size) {
+        maxRadioStation = minRadioStation + size;
+   }
+
+    public void setCurrentRadioStation(int newCurrentRadioStation) {
         if (newCurrentRadioStation > 9) {
             return;
         }
@@ -33,19 +42,6 @@ public class Radio {
     }
 
 
-    public void volumeUp() {
-        if (currentVolume < 10) {
-            currentVolume = currentVolume + 1;
-        }
-    }
-
-    public void volumeDown() {
-        if (currentVolume > 0) {
-            currentVolume = currentVolume - 1;
-        }
-    }
-
-
     public void next() {
         if (currentRadioStation < 9) {
             currentRadioStation = currentRadioStation + 1;
@@ -62,4 +58,51 @@ public class Radio {
         currentVolume = newCurrentVolume;
     }
 
+    public int maxVolume() {
+        if (currentVolume == 100) {
+            currentVolume = currentVolume + 0;
+        }
+        return 0;
+    }
+
+    public short minVolume() {
+        if (currentVolume == 0) {
+            currentVolume = currentVolume - 0;
+        }
+        return 0;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void volumeDown() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void volumeUp() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public int minRadioStation() {
+        return 0;
+    }
+
+    public int maxRadioStation() {
+        return 0;
+    }
+
+    public int getMinVolume () {
+        return  0;
+    }
+
+    public int getMinRadioStation() {
+        return 0;
+    }
 }
