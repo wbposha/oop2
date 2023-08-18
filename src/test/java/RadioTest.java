a139cc18fee5bf35e3711ff013dfcfe7378907a7
wbposha <135736179+wbposha@users.noreply.github.com>
2023-08-19 02:04:25 +0400

    Update RadioTest.java

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import org.example.Radio;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -6,12 +7,184 @@ import org.junit.jupiter.api.Test;
 public class RadioTest {
 
     @Test
-    public void radioTest() {
-        Radio rad = new Radio(9, 0);
+    public void test() {
+        Radio radio = new Radio();
 
-        Assertions.assertEquals(0, rad.minRadioStation());
-        Assertions.assertEquals(0, rad.maxRadioStation());
-        Assertions.assertEquals(0, rad.getCurrentRadioStation());
+        radio.setCurrentStation(8);
+
+        int actual = radio.getCurrentStation();
+        int expected = 8;
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testInvalid() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(-3);
+
+        int actual = radio.getCurrentStation();
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMoreThanPossible() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(30);
+
+        int actual = radio.getCurrentStation();
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPossible() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.setCurrentStation(0);
+
+        int actual = radio.getCurrentStation();
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPossibleSecond() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(9);
+
+        int actual = radio.getCurrentStation();
+        int expected = 9;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testImPossible() {
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(10);
+
+        int actual = radio.getCurrentStation();
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void firstPrevTest() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.prev();
+
+        int actual = radio.getCurrentStation();
+        int expected = 8;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void secondPrevTest() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.prev();
+
+        int actual = radio.getCurrentStation();
+        int expected = 9;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void thirdPrevTest() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(1);
+
+        radio.prev();
+
+        int actual = radio.getCurrentStation();
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countStationsFirstTest() {
+        Radio radio = new Radio(50);
+
+        radio.setCurrentStation(15);
+
+
+        int actual = radio.getCurrentStation();
+        int expected = 15;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countStationsSecondTest() {
+        Radio radio = new Radio(50);
+
+        radio.setCurrentStation(15);
+
+
+        int actual = radio.getCurrentStation();
+        int expected = 15;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countImpossibleStations() {
+        Radio radio = new Radio(50);
+
+        radio.setCurrentStation(51);
+
+
+        int actual = radio.getCurrentStation();
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countPossibleStations() {
+        Radio radio = new Radio(50);
+
+        radio.setCurrentStation(0);
+        radio.next();
+
+
+        int actual = radio.getCurrentStation();
+        int expected = 1;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countPossibleStationsSecondTest() {
+        Radio radio = new Radio(50);
+
+        radio.setCurrentStation(50);
+        radio.prev();
+
+
+        int actual = radio.getCurrentStation();
+        int expected = 49;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countPossibleStationsThirdTest() {
+        Radio radio = new Radio(50);
+
+        radio.setCurrentStation(50);
+        radio.prev();
+
+
+        int actual = radio.getCurrentStation();
+        int expected = 49;
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
+
+
 
